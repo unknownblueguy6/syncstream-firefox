9c3a95d241e8a3def8bc0abc99410ae93762ae3d
Pranav Bhatnagar <unknownblueguy6@gmail.com>
2024-04-25 18:46:23 -0400

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+import svelteParser from "svelte-eslint-parser";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginSvelte.configs["flat/recommended"],
+  {
+    files: ["src/**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+  },
+  eslintConfigPrettier,
+];
